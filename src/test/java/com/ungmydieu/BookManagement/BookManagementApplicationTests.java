c464a8f03e21ad26adc05ec582031115fdd6e184
UNG MY DIEU <mydieu.mdu@gmail.com>
2020-05-27 15:38:59 +0700

    Fix test with CircleCI

--- src/test/java/com/ungmydieu/BookManagement/BookManagementApplicationTests.java
+++ src/test/java/com/ungmydieu/BookManagement/BookManagementApplicationTests.java
@@ -1,13 +1,26 @@
 package com.ungmydieu.BookManagement;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class BookManagementApplicationTests {
+public class BookManagementApplicationTests {
+
+	@Value("${spring.datasource.url}")
+	private String dataSourceUrl;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void test_dataSourceUrl() {
+		Assert.assertEquals(dataSourceUrl, "jdbc:mysql://localhost:3306/book_management_test");
 	}
 
 }
