1b60d813578c3d075e32a0ca9f174e5329e3692f
UNG MY DIEU <mydieu.mdu@gmail.com>
2020-05-28 10:00:47 +0700

    Add Base Converters

---
+++ src/test/java/com/ungmydieu/BookManagement/converters/bases/SampleConverter.java
@@ -0,0 +1,50 @@
+package com.ungmydieu.BookManagement.converters.bases;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class SampleConverter extends Converter<String, Integer> {
+
+    @Override
+    public Integer convert(String source) {
+        return Integer.parseInt(source);
+    }
+}
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@WebAppConfiguration
+@EnableAutoConfiguration(exclude = {GsonAutoConfiguration.class})
+class ConverterTest {
+    @Autowired
+    private SampleConverter sampleConverter;
+
+    @Test
+    public void test_Converter() {
+        String number = "1";
+        Integer result = sampleConverter.convert(number);
+        Assert.assertEquals(result, (Integer) 1);
+    }
+
+    @Test
+    public void test_ConverterList() {
+        String[] numbers = {"1", "2", "3"};
+        List<Integer> result = sampleConverter.convert(Arrays.asList(numbers));
+        Assert.assertEquals(result, Arrays.asList(1, 2, 3));
+    }
+}
+
