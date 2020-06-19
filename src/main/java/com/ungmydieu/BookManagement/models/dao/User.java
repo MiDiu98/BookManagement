8368a9b15d743e26569493581f698d4ad59d3259
UNG MY DIEU <mydieu.mdu@gmail.com>
2020-06-19 09:10:16 +0700

    Config with Transaction

--- src/main/java/com/ungmydieu/BookManagement/models/dao/User.java
+++ src/main/java/com/ungmydieu/BookManagement/models/dao/User.java
@@ -43,7 +43,7 @@ public class User {
 
     private String avatar;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_role",
             joinColumns = @JoinColumn(name = "user_id", nullable = false),
